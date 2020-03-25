8fea2c24e1cd26ba0989c41e76259847535c8bb1
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-03-25 20:56:43 +0900

    add api client by fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,67 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-apollo-typescript
-      </h1>
-      <h2 class="subtitle">
-        My wondrous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div :class="$style.container">
+    <h1>Star wars films</h1>
+    <ul>
+      <li v-for="film in films" :key="film.episodeID">
+        {{ film.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+
+interface Film {
+  episodeID: number
+  title: string
+}
+interface Edge {
+  node: Film
+}
+interface Data {
+  films: Film[]
+}
 
 export default Vue.extend({
-  components: {
-    Logo
+  data(): Data {
+    return {
+      films: []
+    }
+  },
+  async created() {
+    const query = `
+{
+  allFilms(first: 3) {
+    edges {
+      node {
+        episodeID
+        title
+      }
+    }
+  }
+}
+`
+    const res = await fetch(
+      'https://swapi-graphql.netlify.com/.netlify/functions/index',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          query
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then((res) => res.json())
+
+    this.films = res.data.allFilms.edges.map((e: Edge) => ({ ...e.node }))
   }
 })
 </script>
 
-<style>
+<style module>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  margin: 10px;
 }
 </style>
